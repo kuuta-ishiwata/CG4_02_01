eb4f3e95c31eb948e2af9886e602db7b8c47af4b
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-05-08 15:07:35 +0900

    ynity

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animation : MonoBehaviour
+public class playerscript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
