6d6359a2a51ffe84bde47314cc8dc58a2deb996f
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-05-09 10:37:33 +0900

    CG_20_21

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class playerscript : MonoBehaviour
 {
+    public Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 v = rb.velocity;
+        float moveSpeed = 1;
+        float moveSpeed2 = -1;
+        float jump = 5;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            v.x = moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x = moveSpeed2;
+        }
+        else
+        {
+            v.x = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            v.y = jump;
+        }
+        rb.velocity = v;
+
+      
+
     }
 }
